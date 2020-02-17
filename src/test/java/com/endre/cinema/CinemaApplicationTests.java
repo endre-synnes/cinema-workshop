0dee64d0bdaee6cad5823b76a4d1ccebb23b9ee1
Endre Synnes <endre.synnes@byr.oslo.kommune.no>
2020-02-17 22:16:38 +0100

    added RestAssured tests

--- src/test/java/com/endre/cinema/CinemaApplicationTests.java
+++ src/test/java/com/endre/cinema/CinemaApplicationTests.java
@@ -1,16 +1,43 @@
 package com.endre.cinema;
 
+import io.restassured.RestAssured;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertTrue;
+
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CinemaApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @LocalServerPort
+    private int port = 0;
+
+    private String path = "/movies";
+    private int age = 15;
+
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
     }
 
+    @Test
+    public void successfullyGetListOfMovies() {
+
+        var response = given()
+                .get(path)
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .body("size()", greaterThan(0));
+    }
 }
