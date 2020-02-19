4e445f8faca962c521cfdf8c0e61a28ee2ea1180
Endre Synnes <endre.synnes@byr.oslo.kommune.no>
2020-02-19 21:29:06 +0100

    created test for movie service

--- src/test/java/com/endre/cinema/CinemaApplicationTests.java
+++ src/test/java/com/endre/cinema/CinemaApplicationTests.java
@@ -2,15 +2,24 @@ package com.endre.cinema;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CinemaApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+    @Autowired
+    private MovieService movieService;
 
+    @Test
+    public void testThatMovieHasBeenAddedToDatabase() {
+        var numberOfMoviesInDb = movieService.getMovies().size();
+        var movieDto = new MovieDto("Star Wars", 14);
+        movieService.createMovie(movieDto);
+        assertTrue(movieService.getMovies().size() > numberOfMoviesInDb);
+    }
 }
